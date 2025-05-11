b056e891f1c5474443acc66b400889af1a708767
serhii-hl <holenkov.s.s@gmail.com>
2025-05-11 12:13:09 +0300

    Initial Commit
    Initial Commit

---
+++ spring-project/src/main/java/core/basesyntax/spring_project/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package core.basesyntax.spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
---
+++ spring-project/src/test/java/core/basesyntax/spring_project/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package core.basesyntax.spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

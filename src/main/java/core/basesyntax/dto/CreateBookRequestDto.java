86d620772f4dcd4c850d9660a6d0d9b659f0bb98
Serhii Holenkov <holenkov.s.s@gmail.com>
2025-05-19 13:32:35 +0300

    Adjusted according to the mentor`s notes except for validation of some fields

--- src/main/java/core/basesyntax/dto/CreateBookRequestDto.java
+++ src/main/java/core/basesyntax/dto/CreateBookRequestDto.java
@@ -4,6 +4,7 @@ import core.basesyntax.validation.Author;
 import core.basesyntax.validation.Isbn;
 import core.basesyntax.validation.Title;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Getter;
@@ -12,20 +13,20 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     @Title
     private String title;
-    @NotNull
+    @NotBlank
     @Author
     private String author;
-    @NotNull
+    @NotBlank
     @Isbn
     private String isbn;
     @NotNull
     @Min(0)
     private BigDecimal price;
-    @NotNull
+    @NotBlank
     private String description;
-    @NotNull
+    @NotBlank
     private String coverImage;
 }

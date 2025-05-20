f21be00d49cc3672330382bd8a6b3539b50f1114
Serhii Holenkov <holenkov.s.s@gmail.com>
2025-05-20 10:30:09 +0300

    Adjusted according to the mentor`s notes + created put operation in controller

--- src/main/java/core/basesyntax/service/BookService.java
+++ src/main/java/core/basesyntax/service/BookService.java
@@ -3,6 +3,7 @@ package core.basesyntax.service;
 import core.basesyntax.dto.BookDto;
 import core.basesyntax.dto.CreateBookRequestDto;
 import core.basesyntax.model.BookSearchParameters;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface BookService {
@@ -10,7 +11,7 @@ public interface BookService {
 
     BookDto update(CreateBookRequestDto bookRequestDto, Long id);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 

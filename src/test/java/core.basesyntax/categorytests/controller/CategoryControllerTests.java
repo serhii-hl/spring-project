cf8695e762e93a52d92201aafd25e675575e5e08
Serhii Holenkov <holenkov.s.s@gmail.com>
2025-06-17 10:57:52 +0300

    Returned transactional

--- src/test/java/core.basesyntax/categorytests/controller/CategoryControllerTests.java
+++ src/test/java/core.basesyntax/categorytests/controller/CategoryControllerTests.java
@@ -19,12 +19,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlMergeMode;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.shaded.org.apache.commons.lang3.builder.EqualsBuilder;
 
+@SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CategoryControllerTests {
     private static MockMvc mockMvc;
